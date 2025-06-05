8b892e054c76e13726e5f5bc2947cd3f6f1c1040
mark <mark@ex10.org>
2025-06-05 11:36:24 +0100

    Add rpc url

--- onchain_deploy/deploy_contract.sh
+++ onchain_deploy/deploy_contract.sh
@@ -20,6 +20,7 @@ MAX_ORACLE_PRICE_VALIDITY=$(decimal_to_hex $( expr 7 \* 24 \* 60 \* 60 ))
 MAX_FUNDING_INTERVAL=$(decimal_to_hex $( expr 7 \* 24 \* 60 \* 60 ))
 MAX_FUNDING_RATE=$(decimal_to_hex 4294967295)
 CANCEL_DELAY=$(decimal_to_hex $( expr 7 \* 24 \* 60 \* 60 ))
+RPC="https://starknet-sepolia.public.blastapi.io/rpc/v0_8"
 
 echo "FEE_POSITION_PUBLIC_KEY: ${FEE_POSITION_PUBLIC_KEY}"
 echo "LIQUIDATION_FUND_PUBLIC_KEY: ${LIQUIDATION_FUND_PUBLIC_KEY}"
@@ -32,6 +33,7 @@ echo "MAX_PRICE_INTERVAL: ${MAX_PRICE_INTERVAL}"
 echo "MAX_ORACLE_PRICE_VALIDITY: ${MAX_ORACLE_PRICE_VALIDITY}"
 echo "MAX_FUNDING_RATE: ${MAX_FUNDING_RATE}"
 echo "CANCEL_DELAY: ${CANCEL_DELAY}"
+echo "RPC: ${RPC}"
 
 # governance_admin: ContractAddress,
 # upgrade_delay: u64,
@@ -46,8 +48,8 @@ echo "CANCEL_DELAY: ${CANCEL_DELAY}"
 # fee_position_owner_public_key: PublicKey,
 # insurance_fund_position_owner_public_key: PublicKey,
 
-HASH=$(starkli declare -w target/dev/perpetuals_Core.contract_class.json  --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5)
-ADDRESS=$(starkli deploy -w "$HASH" ${GOVERNANCE_ADMIN_ADDRESS} ${UPDATE_DELAY_SECONDS} ${COLLATERAL_ID} ${COLLATERAL_CONTRACT_ADDRESS} ${COLLATERAL_QUANTUM} ${MAX_PRICE_INTERVAL} ${MAX_ORACLE_PRICE_VALIDITY} ${MAX_FUNDING_INTERVAL} ${MAX_FUNDING_RATE} ${CANCEL_DELAY} ${FEE_POSITION_PUBLIC_KEY} ${LIQUIDATION_FUND_PUBLIC_KEY} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5)
+HASH=$(starkli declare --rpc "$RPC" -w target/dev/perpetuals_Core.contract_class.json  --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5)
+ADDRESS=$(starkli deploy --rpc "$RPC" -w "$HASH" ${GOVERNANCE_ADMIN_ADDRESS} ${UPDATE_DELAY_SECONDS} ${COLLATERAL_ID} ${COLLATERAL_CONTRACT_ADDRESS} ${COLLATERAL_QUANTUM} ${MAX_PRICE_INTERVAL} ${MAX_ORACLE_PRICE_VALIDITY} ${MAX_FUNDING_INTERVAL} ${MAX_FUNDING_RATE} ${CANCEL_DELAY} ${FEE_POSITION_PUBLIC_KEY} ${LIQUIDATION_FUND_PUBLIC_KEY} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5)
 echo Contract deployed at $ADDRESS
 
 APP_ROLE_ADMIN_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
@@ -59,13 +61,13 @@ APP_GOVERNOR_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921f
 echo "Registering roles..."
 
 
-starkli invoke -w "$ADDRESS" register_app_role_admin ${APP_ROLE_ADMIN_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
-starkli invoke -w "$ADDRESS" register_operator ${OPERATOR_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
-starkli invoke -w "$ADDRESS" register_governance_admin ${GOVERNANCE_ADMIN_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
-starkli invoke -w "$ADDRESS" register_upgrade_governor ${UPGRADE_GOVERNOR_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
-starkli invoke -w "$ADDRESS" register_security_admin ${SECURITY_ADMIN_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
-starkli invoke -w "$ADDRESS" register_security_agent ${SECURITY_AGENT_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
-starkli invoke -w "$ADDRESS" register_app_governor ${APP_GOVERNOR_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
+starkli invoke --rpc "$RPC" -w "$ADDRESS" register_app_role_admin ${APP_ROLE_ADMIN_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
+starkli invoke --rpc "$RPC" -w "$ADDRESS" register_operator ${OPERATOR_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
+starkli invoke --rpc "$RPC" -w "$ADDRESS" register_governance_admin ${GOVERNANCE_ADMIN_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
+starkli invoke --rpc "$RPC" -w "$ADDRESS" register_upgrade_governor ${UPGRADE_GOVERNOR_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
+starkli invoke --rpc "$RPC" -w "$ADDRESS" register_security_admin ${SECURITY_ADMIN_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
+starkli invoke --rpc "$RPC" -w "$ADDRESS" register_security_agent ${SECURITY_AGENT_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
+starkli invoke --rpc "$RPC" -w "$ADDRESS" register_app_governor ${APP_GOVERNOR_ADDRESS} --account onchain_deploy/testnet_keys/account.json --private-key 0x06c73b5813f1cdb4051eedfcf49f28285d062bf59d9f03a88cab147a1a856ce5
 
 # echo "Contract deployed and initialised at address: ${ADDRESS}"
 
