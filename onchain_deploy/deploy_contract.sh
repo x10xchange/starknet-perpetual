0a41d8a6aab6c48d997577531e6eaf3051a39725
stefano <stefano.franz@me.com>
2025-12-03 18:03:02 +0000

    set app gov

--- onchain_deploy/deploy_contract.sh
+++ onchain_deploy/deploy_contract.sh
@@ -26,67 +26,67 @@ then
    helpFunction
 fi
 
-FEE_POSITION_PUBLIC_KEY="0x4313efb47c1e488ad57e9b66ff11a941310e23307d8fdbc26b2795199bcb57a"
-LIQUIDATION_FUND_PUBLIC_KEY="0x4313efb47c1e488ad57e9b66ff11a941310e23307d8fdbc26b2795199bcb57a"
-GOVERNANCE_ADMIN_ADDRESS="0x19ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
-UPDATE_DELAY_SECONDS=$(decimal_to_hex 0)
-COLLATERAL_ID="0x31857064564ed0ff978e687456963cba09c2c6985d8f9300a1de4962fafa054"
-COLLATERAL_CONTRACT_ADDRESS="0x5ba91db44b3e6a4485b5dbfcb17d791faa9cb6890a42731b66b3536b28b8ed5"
-COLLATERAL_QUANTUM=$(decimal_to_hex 1)
-MAX_PRICE_INTERVAL=$(decimal_to_hex $( expr 7 \* 24 \* 60 \* 60 ))
-MAX_ORACLE_PRICE_VALIDITY=$(decimal_to_hex $( expr 7 \* 24 \* 60 \* 60 ))
-MAX_FUNDING_INTERVAL=$(decimal_to_hex $( expr 7 \* 24 \* 60 \* 60 ))
-MAX_FUNDING_RATE=$(decimal_to_hex 4294967295)
-CANCEL_DELAY=$(decimal_to_hex $( expr 7 \* 24 \* 60 \* 60 ))
-RPC="https://starknet-sepolia.public.blastapi.io/rpc/v0_8"
+# FEE_POSITION_PUBLIC_KEY="0x4313efb47c1e488ad57e9b66ff11a941310e23307d8fdbc26b2795199bcb57a"
+# LIQUIDATION_FUND_PUBLIC_KEY="0x4313efb47c1e488ad57e9b66ff11a941310e23307d8fdbc26b2795199bcb57a"
+# GOVERNANCE_ADMIN_ADDRESS="0x19ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
+# UPDATE_DELAY_SECONDS=$(decimal_to_hex 0)
+# COLLATERAL_ID="0x31857064564ed0ff978e687456963cba09c2c6985d8f9300a1de4962fafa054"
+# COLLATERAL_CONTRACT_ADDRESS="0x5ba91db44b3e6a4485b5dbfcb17d791faa9cb6890a42731b66b3536b28b8ed5"
+# COLLATERAL_QUANTUM=$(decimal_to_hex 1)
+# MAX_PRICE_INTERVAL=$(decimal_to_hex $( expr 7 \* 24 \* 60 \* 60 ))
+# MAX_ORACLE_PRICE_VALIDITY=$(decimal_to_hex $( expr 7 \* 24 \* 60 \* 60 ))
+# MAX_FUNDING_INTERVAL=$(decimal_to_hex $( expr 7 \* 24 \* 60 \* 60 ))
+# MAX_FUNDING_RATE=$(decimal_to_hex 4294967295)
+# CANCEL_DELAY=$(decimal_to_hex $( expr 7 \* 24 \* 60 \* 60 ))
+# RPC="https://starknet-sepolia.public.blastapi.io/rpc/v0_8"
 
-echo "FEE_POSITION_PUBLIC_KEY: ${FEE_POSITION_PUBLIC_KEY}"
-echo "LIQUIDATION_FUND_PUBLIC_KEY: ${LIQUIDATION_FUND_PUBLIC_KEY}"
-echo "GOVERNANCE_ADMIN_ADDRESS: ${GOVERNANCE_ADMIN_ADDRESS}"
-echo "UPDATE_DELAY_SECONDS: ${UPDATE_DELAY_SECONDS}"
-echo "COLLATERAL_ID: ${COLLATERAL_ID}"
-echo "COLLATERAL_CONTRACT_ADDRESS: ${COLLATERAL_CONTRACT_ADDRESS}"
-echo "COLLATERAL_QUANTUM: ${COLLATERAL_QUANTUM}"
-echo "MAX_PRICE_INTERVAL: ${MAX_PRICE_INTERVAL}"
-echo "MAX_ORACLE_PRICE_VALIDITY: ${MAX_ORACLE_PRICE_VALIDITY}"
-echo "MAX_FUNDING_RATE: ${MAX_FUNDING_RATE}"
-echo "CANCEL_DELAY: ${CANCEL_DELAY}"
-echo "RPC: ${RPC}"
+# echo "FEE_POSITION_PUBLIC_KEY: ${FEE_POSITION_PUBLIC_KEY}"
+# echo "LIQUIDATION_FUND_PUBLIC_KEY: ${LIQUIDATION_FUND_PUBLIC_KEY}"
+# echo "GOVERNANCE_ADMIN_ADDRESS: ${GOVERNANCE_ADMIN_ADDRESS}"
+# echo "UPDATE_DELAY_SECONDS: ${UPDATE_DELAY_SECONDS}"
+# echo "COLLATERAL_ID: ${COLLATERAL_ID}"
+# echo "COLLATERAL_CONTRACT_ADDRESS: ${COLLATERAL_CONTRACT_ADDRESS}"
+# echo "COLLATERAL_QUANTUM: ${COLLATERAL_QUANTUM}"
+# echo "MAX_PRICE_INTERVAL: ${MAX_PRICE_INTERVAL}"
+# echo "MAX_ORACLE_PRICE_VALIDITY: ${MAX_ORACLE_PRICE_VALIDITY}"
+# echo "MAX_FUNDING_RATE: ${MAX_FUNDING_RATE}"
+# echo "CANCEL_DELAY: ${CANCEL_DELAY}"
+# echo "RPC: ${RPC}"
 
-# governance_admin: ContractAddress,
-# upgrade_delay: u64,
-# collateral_id: AssetId,
-# collateral_token_address: ContractAddress,
-# collateral_quantum: u64,
-# max_price_interval: TimeDelta,
-# max_oracle_price_validity: TimeDelta,
-# max_funding_interval: TimeDelta,
-# max_funding_rate: u32,
-# cancel_delay: TimeDelta,
-# fee_position_owner_public_key: PublicKey,
-# insurance_fund_position_owner_public_key: PublicKey,
-pwd
-HASH=$(./onchain_deploy/declare.sh -a $account -u $url -c Core -p perpetuals)
-echo Hash is $HASH
-ADDRESS=$(sncast --account $account  deploy -u $url --class-hash=$HASH  --constructor-calldata ${GOVERNANCE_ADMIN_ADDRESS} ${UPDATE_DELAY_SECONDS} ${COLLATERAL_ID} ${COLLATERAL_CONTRACT_ADDRESS} ${COLLATERAL_QUANTUM} ${MAX_PRICE_INTERVAL} ${MAX_ORACLE_PRICE_VALIDITY} ${MAX_FUNDING_INTERVAL} ${MAX_FUNDING_RATE} ${CANCEL_DELAY} ${FEE_POSITION_PUBLIC_KEY} ${LIQUIDATION_FUND_PUBLIC_KEY} | grep -oE '0x[0-9a-f]{64}'| sed -n '1p')
-echo Contract deployed at $ADDRESS
-sleep 10
+# # governance_admin: ContractAddress,
+# # upgrade_delay: u64,
+# # collateral_id: AssetId,
+# # collateral_token_address: ContractAddress,
+# # collateral_quantum: u64,
+# # max_price_interval: TimeDelta,
+# # max_oracle_price_validity: TimeDelta,
+# # max_funding_interval: TimeDelta,
+# # max_funding_rate: u32,
+# # cancel_delay: TimeDelta,
+# # fee_position_owner_public_key: PublicKey,
+# # insurance_fund_position_owner_public_key: PublicKey,
+# pwd
+# HASH=$(./onchain_deploy/declare.sh -a $account -u $url -c Core -p perpetuals)
+# echo Hash is $HASH
+# ADDRESS=$(sncast --account $account  deploy -u $url --class-hash=$HASH  --constructor-calldata ${GOVERNANCE_ADMIN_ADDRESS} ${UPDATE_DELAY_SECONDS} ${COLLATERAL_ID} ${COLLATERAL_CONTRACT_ADDRESS} ${COLLATERAL_QUANTUM} ${MAX_PRICE_INTERVAL} ${MAX_ORACLE_PRICE_VALIDITY} ${MAX_FUNDING_INTERVAL} ${MAX_FUNDING_RATE} ${CANCEL_DELAY} ${FEE_POSITION_PUBLIC_KEY} ${LIQUIDATION_FUND_PUBLIC_KEY} | grep -oE '0x[0-9a-f]{64}'| sed -n '1p')
+# echo Contract deployed at $ADDRESS
+# sleep 10
 
-APP_ROLE_ADMIN_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
-OPERATOR_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
-UPGRADE_GOVERNOR_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
-SECURITY_ADMIN_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
-SECURITY_AGENT_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
-APP_GOVERNOR_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
-echo "Registering roles..."
+# APP_ROLE_ADMIN_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
+# OPERATOR_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
+# UPGRADE_GOVERNOR_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
+# SECURITY_ADMIN_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
+# SECURITY_AGENT_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
+# APP_GOVERNOR_ADDRESS="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
+# echo "Registering roles..."
 
-sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_app_role_admin --calldata  ${APP_ROLE_ADMIN_ADDRESS}
-sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_operator --calldata  ${OPERATOR_ADDRESS}
-sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_governance_admin --calldata  ${GOVERNANCE_ADMIN_ADDRESS}
-sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_upgrade_governor --calldata  ${UPGRADE_GOVERNOR_ADDRESS}
-sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_security_admin --calldata  ${SECURITY_ADMIN_ADDRESS}
-sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_security_agent --calldata  ${SECURITY_AGENT_ADDRESS}
-sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_app_governor --calldata  ${APP_GOVERNOR_ADDRESS}
+# sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_app_role_admin --calldata  ${APP_ROLE_ADMIN_ADDRESS}
+# sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_operator --calldata  ${OPERATOR_ADDRESS}
+# sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_governance_admin --calldata  ${GOVERNANCE_ADMIN_ADDRESS}
+# sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_upgrade_governor --calldata  ${UPGRADE_GOVERNOR_ADDRESS}
+# sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_security_admin --calldata  ${SECURITY_ADMIN_ADDRESS}
+# sncast --account $account invoke -u $url --contract-address $ADDRESS --function register_security_agent --calldata  ${SECURITY_AGENT_ADDRESS}
+sncast --account $account invoke -u $url --contract-address 0x05F062C924EcD9f5d4C74c567A28cc5502332fcf21828687EC20581a03F7E1C8 --function register_app_governor --calldata  0x030c48A027CCcD0A89262Ac3a0E92973cE206B206160380200FD68FB19B80c88
 
 #TRANSFER_COMPONENT_HASH=$(./onchain_deploy/declare.sh -a $account -u $url -c TransferManager -p perpetuals)
 #echo "transfer hash $TRANSFER_COMPONENT_HASH"
