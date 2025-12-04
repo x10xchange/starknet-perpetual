67771a466dfcbf3dec15bd8d617b3aea30f93754
mark <mark@ex10.org>
2025-12-04 11:36:13 +0000

    Deploy vault

--- utils/deploy_vault.sh
+++ utils/deploy_vault.sh
@@ -46,7 +46,7 @@ COLLATERAL_CONTRACT_ADDRESS="0x5ba91db44b3e6a4485b5dbfcb17d791faa9cb6890a42731b6
 PERPS_CONTRACT="0x05F062C924EcD9f5d4C74c567A28cc5502332fcf21828687EC20581a03F7E1C8"
 OWNING_POSITION="0x7"
 RECIPIENT="0x019ec96d4aea6fdc6f0b5f393fec3f186aefa8f0b8356f43d07b921ff48aa5da"
-INITIAL_PRICE=$(decimal_to_hex 1000000)
+INITIAL_PRICE=$(decimal_to_hex 993063)
 HASH=$(./utils/declare.sh -a $account -u $url -c ProtocolVault -p vault)
 echo Hash is $HASH
 ADDRESS=$(sncast --account $account  deploy -u $url --class-hash=$HASH  --constructor-calldata $NAME $SYMBOL $COLLATERAL_CONTRACT_ADDRESS $PERPS_CONTRACT $OWNING_POSITION $RECIPIENT $INITIAL_PRICE | grep -oE '0x[0-9a-f]{64}'| sed -n '1p')
